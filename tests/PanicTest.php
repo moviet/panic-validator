30ebb30c9187bad79eeedc4408ccec438e66228f
Moviet <snorthfed@gmail.com>
2018-12-14 11:48:32 +0700

    fixing line config

--- tests/PanicTest.php
+++ tests/PanicTest.php
@@ -14,315 +14,315 @@ use PHPUnit\Framework\TestCase;
 
 class PanicTest extends TestCase
 {
-		public $valid;
+    public $valid;
 
-		public function setUp()
-		{
-				$this->valid = new Panic;
+    public function setUp()
+    {
+        $this->valid = new Panic;
 
-				return $this->valid;
-		}
+        return $this->valid;
+    }
 
-		public function testCaseCannotBeEmpty()
-		{
-				$mock = $this->createMock(Panic::class);
+    public function testCaseCannotBeEmpty()
+    {
+        $mock = $this->createMock(Panic::class);
 
-				$mock->method('case')
-						 ->willReturn('object_validation');
-		
-				$this->assertEquals('object_validation', $mock->case());
+        $mock->method('case')
+            ->willReturn('object_validation');
 
-				$null = empty($mock->case());
+        $this->assertEquals('object_validation', $mock->case());
 
-				$this->assertFalse($null);
-		}
+        $null = empty($mock->case());
 
-		public function testWithDefaultLang()
-		{
-				$mock = $this->createMock(Panic::class);
+        $this->assertFalse($null);
+    }
 
-				$mock->expects(self::any())
-						 ->method('lang')
-						 ->with('En')
-						 ->will($this->returnValue('En'));
-		
-				$this->assertEquals('En',$mock->lang('En'));
+    public function testWithDefaultLang()
+    {
+        $mock = $this->createMock(Panic::class);
 
-				$null = is_null($mock->lang('En'));
+        $mock->expects(self::any())
+             ->method('lang')
+             ->with('En')
+             ->will($this->returnValue('En'));
 
-				$this->assertFalse($null);
-		}
+        $this->assertEquals('En',$mock->lang('En'));
 
-		public function testMinMustBeNumeric()
-		{
-				$mock = $this->createMock(Panic::class);
+        $null = is_null($mock->lang('En'));
 
-				$mock->expects(self::any())
-						 ->method('min')
-						 ->will($this->returnValue(9));
+        $this->assertFalse($null);
+    }
 
-				$this->assertEquals(9, $mock->min(9));
+    public function testMinMustBeNumeric()
+    {
+        $mock = $this->createMock(Panic::class);
 
-				$num = is_string($mock->min(9));
+        $mock->expects(self::any())
+             ->method('min')
+             ->will($this->returnValue(9));
 
-				$this->assertFalse($num);
-		}
+        $this->assertEquals(9, $mock->min(9));
 
-		public function testMaxMustBeNumeric()
-		{
-				$mock = $this->createMock(Panic::class);
+        $num = is_string($mock->min(9));
 
-				$mock->expects(self::any())
-						 ->method('max')
-						 ->will($this->returnValue(10));
+        $this->assertFalse($num);
+    }
 
-				$this->assertEquals(10, $mock->max(10));
+    public function testMaxMustBeNumeric()
+    {
+        $mock = $this->createMock(Panic::class);
 
-				$num = is_string($mock->max(10));
+        $mock->expects(self::any())
+             ->method('max')
+             ->will($this->returnValue(10));
 
-				$this->assertFalse($num);
-		}
+        $this->assertEquals(10, $mock->max(10));
 
-		public function testAuthMustBeNumeric()
-		{
-				$mock = $this->createMock(Panic::class);
+        $num = is_string($mock->max(10));
 
-				$mock->expects(self::any())
-						 ->method('auth')
-						 ->will($this->returnValue(8));
+        $this->assertFalse($num);
+    }
 
-				$this->assertEquals(8, $mock->auth(8));
+    public function testAuthMustBeNumeric()
+    {
+        $mock = $this->createMock(Panic::class);
 
-				$num = is_string($mock->auth(8));
+        $mock->expects(self::any())
+             ->method('auth')
+             ->will($this->returnValue(8));
 
-				$this->assertFalse($num);
-		}
+        $this->assertEquals(8, $mock->auth(8));
 
-		public function testRuleMustBeStringNotNull()
-		{
-				$mock = $this->createMock(Panic::class);
+        $num = is_string($mock->auth(8));
 
-				$mock->expects(self::any())
-						 ->method('rule')
-						 ->will($this->returnValue(':num'));
+        $this->assertFalse($num);
+    }
 
-				$this->assertEquals(':num', $mock->rule(':num'));
+    public function testRuleMustBeStringNotNull()
+    {
+        $mock = $this->createMock(Panic::class);
 
-				$num = is_int($mock->rule(':num'));
+        $mock->expects(self::any())
+             ->method('rule')
+             ->will($this->returnValue(':num'));
 
-				$null = !is_null($mock->rule(':num'));
+        $this->assertEquals(':num', $mock->rule(':num'));
 
-				$this->assertFalse($num);
+        $num = is_int($mock->rule(':num'));
 
-				$this->assertTrue($null);
-		}
+        $null = !is_null($mock->rule(':num'));
 
-		public function testValidateMinimumRequirementFailure()
-		{
-				$validate = $this->setUp()->case('6289020200') // +6289020200 (+)
-																	->min(7)
-																	->rule(':phone')
-																	->throw(['Must Be Numeric']);
+        $this->assertFalse($num);
 
-				$valid = $this->setUp()->confirm([$validate]);
+        $this->assertTrue($null);
+    }
 
-				$this->assertFalse($valid);
-		}
+    public function testValidateMinimumRequirementFailure()
+    {
+        $validate = $this->setUp()->case('6289020200') // +6289020200 (+)
+                                ->min(7)
+                                ->rule(':phone')
+                                ->throw(['Must Be Numeric']);
 
-		public function testValidateMaximumRequirementFailure()
-		{
-				$validate = $this->setUp()->case('inject this')
-																	->min(2)
-																	->max(7)
-																	->rule(':alphaSpace')
-																	->throw(['Must Be Numeric']);
+        $valid = $this->setUp()->confirm([$validate]);
 
-				$valid = $this->setUp()->confirm([$validate]);
+        $this->assertFalse($valid);
+    }
 
-				$this->assertFalse($valid);
-		}
+    public function testValidateMaximumRequirementFailure()
+    {
+        $validate = $this->setUp()->case('inject this')
+                                ->min(2)
+                                ->max(7)
+                                ->rule(':alphaSpace')
+                                ->throw(['Must Be Numeric']);
 
-		public function testValidateMinPasswordLengthFailure()
-		{
-				$validate = $this->setUp()->case('inject')
-																	->auth(8)
-																	->throw(['Password Minimum 8 Characters']);
+        $valid = $this->setUp()->confirm([$validate]);
 
-				$valid = $this->setUp()->confirm([$validate]);
+        $this->assertFalse($valid);
+    }
 
-				$this->assertTrue($valid);
-		}
+    public function testValidateMinPasswordLengthFailure()
+    {
+        $validate = $this->setUp()->case('inject')
+                                ->auth(8)
+                                ->throw(['Password Minimum 8 Characters']);
 
-		public function testValidationThrowSuccess()
-		{
-				$mock = $this->createMock(Panic::class);
-				
-				$mock->expects(self::any())
-						 ->method('throw')
-						 ->will($this->returnValue(null));
+        $valid = $this->setUp()->confirm([$validate]);
 
-				$validate = $this->setUp()->case(12345)->rule(':num')->throw(['Must Be Numeric']);
+        $this->assertTrue($valid);
+    }
 
-				$valid = $this->setUp()->confirm([$validate]);
+    public function testValidationThrowSuccess()
+    {
+        $mock = $this->createMock(Panic::class);
 
-				$this->assertEquals($validate, $mock->throw(null));
+        $mock->expects(self::any())
+             ->method('throw')
+             ->will($this->returnValue(null));
 
-				$this->assertTrue($valid);
-		}
+        $validate = $this->setUp()->case(12345)->rule(':num')->throw(['Must Be Numeric']);
 
-		public function testValidationWithCallback()
-		{
-				$image = 'file.jpg';
+        $valid = $this->setUp()->confirm([$validate]);
 
-				$validate = $this->setUp()->case($image)->rule(':image')->get();
+        $this->assertEquals($validate, $mock->throw(null));
 
-				$this->assertEquals($validate, $image);
-		}
+        $this->assertTrue($valid);
+    }
 
-		public function testValidationThrowFailure()
-		{
-				$mock = $this->createMock(Panic::class);
+    public function testValidationWithCallback()
+    {
+        $image = 'file.jpg';
 
-				$mock->expects(self::any())
-						 ->method('throw')
-						 ->will($this->returnValue(null));
+        $validate = $this->setUp()->case($image)->rule(':image')->get();
 
-				$validate = $this->setUp()->case(12345)->rule(':alpha')->throw(['Must Be Alphabet']);
+        $this->assertEquals($validate, $image);
+    }
 
-				$valid = $this->setUp()->confirm([$validate]);
+    public function testValidationThrowFailure()
+    {
+        $mock = $this->createMock(Panic::class);
 
-				$this->assertNotEquals($validate, $mock->throw(null));
+        $mock->expects(self::any())
+             ->method('throw')
+             ->will($this->returnValue(null));
 
-				$this->assertFalse($valid);
-		}
+        $validate = $this->setUp()->case(12345)->rule(':alpha')->throw(['Must Be Alphabet']);
 
-		public function testValidationWithPatternFailure()
-		{
-				$url = 'test@email..com';
+        $valid = $this->setUp()->confirm([$validate]);
 
-				$validate = $this->setUp()->match(':email',$url);
+        $this->assertNotEquals($validate, $mock->throw(null));
 
-				$this->assertFalse($validate);
-		}
+        $this->assertFalse($valid);
+    }
 
-		public function testValidationWithFilter()
-		{
-				$url = 'https://github.com/moviet/panic-validator';
+    public function testValidationWithPatternFailure()
+    {
+        $url = 'test@email..com';
 
-				$validate = $this->setUp()->filter(':url',$url);
+        $validate = $this->setUp()->match(':email',$url);
 
-				$this->assertEquals($validate, $url);
-		}
+        $this->assertFalse($validate);
+    }
 
-		public function testValidationWithYourOwnCustomPattern()
-		{
-				$myrule = $this->setUp()->case('My name is Rara')
-																->lang('Id')
-																->min(2)
-																->max(20)
-																->modify('/^[a-zA-Z 0-9]*$/')
-																->throw(['something wrong']);
+    public function testValidationWithFilter()
+    {
+        $url = 'https://github.com/moviet/panic-validator';
 
-				$this->assertNull($myrule);
-		}
+        $validate = $this->setUp()->filter(':url',$url);
 
-		public function testValidationWithCustomRegex()
-		{
-				$string = 'Hello Foo';
+        $this->assertEquals($validate, $url);
+    }
 
-				$validate = $this->setUp()->draft('/^[a-zA-Z ]*$/',$string);
+    public function testValidationWithYourOwnCustomPattern()
+    {
+        $myrule = $this->setUp()->case('My name is Rara')
+                                ->lang('Id')
+                                ->min(2)
+                                ->max(20)
+                                ->modify('/^[a-zA-Z 0-9]*$/')
+                                ->throw(['something wrong']);
 
-				$this->assertEquals($validate, $string);
-		}
+        $this->assertNull($myrule);
+    }
 
-		public function testValidationCompare()
-		{
-				$string = 'Hello Foo';
+    public function testValidationWithCustomRegex()
+    {
+        $string = 'Hello Foo';
 
-				$validate = $this->setUp()->draft('/^[a-zA-Z ]*$/',$string);
+        $validate = $this->setUp()->draft('/^[a-zA-Z ]*$/',$string);
 
-				$equal = $this->setUp()->equal($validate, $string);
+        $this->assertEquals($validate, $string);
+    }
 
-				$this->assertTrue($equal);
-		}
+    public function testValidationCompare()
+    {
+        $string = 'Hello Foo';
 
-		public function testValidationCompareHashEqual()
-		{
-				$string = 'a23cabadaed728278badbe0100920ababdcbe204940faefan030';
+        $validate = $this->setUp()->draft('/^[a-zA-Z ]*$/',$string);
 
-				$validate = $this->setUp()->draft('/^[a-zA-Z0-9]*$/',$string);
+        $equal = $this->setUp()->equal($validate, $string);
 
-				$equal = $this->setUp()->hashEqual($validate, $string);
+        $this->assertTrue($equal);
+    }
 
-				$this->assertTrue($equal);
-		}
+    public function testValidationCompareHashEqual()
+    {
+        $string = 'a23cabadaed728278badbe0100920ababdcbe204940faefan030';
 
-		public function testValidationPassword()
-		{
-				$password = 'This is my password';
+        $validate = $this->setUp()->draft('/^[a-zA-Z0-9]*$/',$string);
 
-				$validate = $this->setUp()->verify($password,'$2y$14$cSUK14J/Uz8WroJyeagwx.grUaKNX85NbxC3z96nLJQ0aItIjTBbe')->warn('Password Invalid');
+        $equal = $this->setUp()->hashEqual($validate, $string);
 
-				if ($this->setUp()->catch($validate)) {
+        $this->assertTrue($equal);
+    }
 
-						$this->assertTrue($validate);
-				
-				} else {
+    public function testValidationPassword()
+    {
+        $password = 'This is my password';
 
-						$this->assertFalse($validate);
-				}
-		}
+        $validate = $this->setUp()->verify($password,'$2y$14$cSUK14J/Uz8WroJyeagwx.grUaKNX85NbxC3z96nLJQ0aItIjTBbe')->warn('Password Invalid');
 
-		public function testValidationUsingErrorMessageAndCallback()
-		{
-				$post = [12345,123456];
+        if ($this->setUp()->catch($validate)) {
 
-				$validate = $this->setUp()->case($post[0])->rule(':num')->throw(['Must Be Alphabet']);
+        $this->assertTrue($validate);
 
-				$validate2 = $this->setUp()->case($post[1])->rule(':num')->throw(['Must Be Alphabet']);
+        } else {
 
-				$val = $this->setUp()->trust($post, [$validate,$validate2]);
+            $this->assertFalse($validate);
+        }
+    }
 
-				$this->assertEquals($val[0], $post[0]);
-		}
+    public function testValidationUsingErrorMessageAndCallback()
+    {
+        $post = [12345,123456];
 
-		public function testEncodingNiceBase64()
-		{
-				$encode = $this->setUp()->base64('this is panic tests');
+        $validate = $this->setUp()->case($post[0])->rule(':num')->throw(['Must Be Alphabet']);
 
-				$decode = $this->setUp()->pure64($encode);
+        $validate2 = $this->setUp()->case($post[1])->rule(':num')->throw(['Must Be Alphabet']);
 
-				$this->assertEquals('this is panic tests', $decode);
-		}
+        $val = $this->setUp()->trust($post, [$validate,$validate2]);
 
-		public function testEncodingBase64Failure()
-		{
-				$encode = 'dGhpcyBpcyBwYW5pYyB0ZXN0cwww';
+        $this->assertEquals($val[0], $post[0]);
+    }
 
-				$decode = $this->setUp()->pure64($encode);
+    public function testEncodingNiceBase64()
+    {
+        $encode = $this->setUp()->base64('this is panic tests');
 
-				$this->assertNotEquals('this is panic tests', $decode);
-		}
+        $decode = $this->setUp()->pure64($encode);
 
-		public function testEncodingHtmlSuccess()
-		{
-				$encodeHtml = '<a href=https://github.com/moviet/panic-validator>panic validator</a><script>XSS</script>';
+        $this->assertEquals('this is panic tests', $decode);
+    }
 
-				$encode = $this->setUp()->htmlSafe($encodeHtml);
+    public function testEncodingBase64Failure()
+    {
+        $encode = 'dGhpcyBpcyBwYW5pYyB0ZXN0cwww';
 
-				$this->assertNotEquals($encodeHtml, $encode);
-		}
+        $decode = $this->setUp()->pure64($encode);
 
-		public function testDecodingHtmlSuccess()
-		{
-				$encodeHtml = '<a href=https://github.com/moviet/panic-validator>panic validator</a><script>XSS</script>';
+        $this->assertNotEquals('this is panic tests', $decode);
+    }
 
-				$encode = $this->setUp()->htmlSafe($encodeHtml);
+    public function testEncodingHtmlSuccess()
+    {
+        $encodeHtml = '<a href=https://github.com/moviet/panic-validator>panic validator</a><script>XSS</script>';
 
-				$decode = $this->setUp()->htmlRaw($encode);
+        $encode = $this->setUp()->htmlSafe($encodeHtml);
 
-				$this->assertSame($encodeHtml, $decode);
-		}
+        $this->assertNotEquals($encodeHtml, $encode);
+    }
 
-		public function tearDown() {}
+    public function testDecodingHtmlSuccess()
+    {
+        $encodeHtml = '<a href=https://github.com/moviet/panic-validator>panic validator</a><script>XSS</script>';
+
+        $encode = $this->setUp()->htmlSafe($encodeHtml);
+
+        $decode = $this->setUp()->htmlRaw($encode);
+
+        $this->assertSame($encodeHtml, $decode);
+    }
+
+    public function tearDown() {}
 }
